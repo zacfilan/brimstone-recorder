d6e65862952a77d97d4b4f72b74f9736f37445a7
patchworksapp <patchworksapp@gmail.com>
2021-09-08 14:39:26 -0400

    more obvious interaction on failure

--- ui/card.js
+++ ui/card.js
@@ -279,7 +279,7 @@ export class TestAction {
 
         let html = `
     <div class='card ${this.classes()} ${className}' data-index=${this.index}>
-        <div title='Click to cycle through\nexpected, actual, and difference views.' class='click-to-change-view title'><div style="float:left;">${title}</div><div style="float:right;">${this.index + 1}</div></div>
+        <div title='Click to cycle through\nexpected, actual, and difference views.' class='click-to-change-view title'>${title}<div class="stepNumber">${this.index + 1}</div></div>
         <div class="meter">
             <span style="width:100%;"><span class="progress"></span></span>
             <span style="width:100%;"><span class="match-status"></span></span>
@@ -349,7 +349,7 @@ export class Step {
 
         if (this.next) {
             let src;
-            let title = '<span>';
+            let title = '';
 
             if(this.next._match === constants.match.PLAY) {
                 title += 'Wait for actual to match.';
@@ -357,7 +357,8 @@ export class Step {
             else {
 
                 if(this.next._match === constants.match.FAIL) {
-                    title += '❌ failed match. ';
+                    title += '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exchange" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-exchange fa-w-16 fa-5x"><path fill="currentColor" d="M0 168v-16c0-13.255 10.745-24 24-24h381.97l-30.467-27.728c-9.815-9.289-10.03-24.846-.474-34.402l10.84-10.84c9.373-9.373 24.568-9.373 33.941 0l82.817 82.343c12.497 12.497 12.497 32.758 0 45.255l-82.817 82.343c-9.373 9.373-24.569 9.373-33.941 0l-10.84-10.84c-9.556-9.556-9.341-25.114.474-34.402L405.97 192H24c-13.255 0-24-10.745-24-24zm488 152H106.03l30.467-27.728c9.815-9.289 10.03-24.846.474-34.402l-10.84-10.84c-9.373-9.373-24.568-9.373-33.941 0L9.373 329.373c-12.497 12.497-12.497 32.758 0 45.255l82.817 82.343c9.373 9.373 24.569 9.373 33.941 0l10.84-10.84c9.556-9.556 9.341-25.113-.474-34.402L106.03 384H488c13.255 0 24-10.745 24-24v-16c0-13.255-10.745-24-24-24z" class=""></path></svg>'
+                    title += ' failed match. ';
                 }
 
                 switch (this.next._view) {
@@ -383,7 +384,6 @@ export class Step {
                 title += ` <span id='allowed-differences'>Has allowed differences.</span>`;
             }
 
-            title += '</span>';
             html += this.next.toHtml({ title: title, src: src, className: 'waiting'});
         }
         html += `
